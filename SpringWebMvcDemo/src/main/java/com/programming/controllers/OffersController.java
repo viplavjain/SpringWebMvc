fdeb189e01ab54c6bccf49d7d75d4b5715d0115e
viplav.jain <viplav.jain@DEL1-LHP-N61492.synapse.com>
2019-12-30 23:50:05 +0530

    commit2

---
+++ SpringWebMvcDemo/src/main/java/com/programming/controllers/OffersController.java
@@ -0,0 +1,16 @@
+package com.programming.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController 
+{
+	@RequestMapping("/")
+	public String showHome()
+	{
+		return "home";
+	}
+	
+
+}
